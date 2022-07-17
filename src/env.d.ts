3b114774bc3575d11c7c56a29ce61c848bf3e856
fredafei <964959033@qq.com>
2022-07-17 11:51:51 +0800

    清除无用文件

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,9 +6,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-declare module '*.tsx' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export {component}
-}
