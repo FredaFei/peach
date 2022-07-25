4460daaffbb006ddcadbb16461ca7836dc4cebce
fredafei <964959033@qq.com>
2022-07-26 07:08:47 +0800

    useSwipe 添加事件hook, welcome使用该hook时阻止默认事件，只让card滑动，welcome route添加name

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -8,6 +8,7 @@ import { SecondActions } from '../components/welcome/SecondActions'
 import { ThirdActions } from '../components/welcome/ThirdActions'
 import { ForthActions } from '../components/welcome/ForthActions'
 import { Welcome } from '../views/welcome/Welcome'
+import { StartPage } from '../views/start/StartPage'
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/welcome' },
@@ -16,10 +17,10 @@ export const routes: RouteRecordRaw[] = [
     component: Welcome,
     children: [
       { path: '', redirect: '/welcome/1' },
-      { path: '1', components: { main: First, footer: FirstActions } },
-      { path: '2', components: { main: Second, footer: SecondActions } },
-      { path: '3', components: { main: Third, footer: ThirdActions } },
-      { path: '4', components: { main: Forth, footer: ForthActions } },
+      { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions } },
+      { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions } },
+      { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions } },
+      { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions } },
     ],
   },
 ]
