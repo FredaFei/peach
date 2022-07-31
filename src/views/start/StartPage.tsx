16afc977bfe4602dabc14e3e725185810f442183
fredafei <964959033@qq.com>
2022-07-31 14:34:02 +0800

    start 添加overlay 组件

--- src/views/start/StartPage.tsx
+++ src/views/start/StartPage.tsx
@@ -1,12 +1,14 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import { Button } from '../../components/button/Button';
 import { Center } from '../../components/center/Center';
 import { FloatButton } from '../../components/float-button/FloatButton';
 import { Icon } from '../../components/icon/Icon';
 import { Navbar } from '../../components/navbar/Navbar';
+import { Overlay } from '../../components/overlay/Overlay';
 import s from './StartPage.module.scss';
 export const StartPage = defineComponent({
   setup: (props, context) => {
+    const refVisibleOverlay = ref(false);
     const onClick = (e: MouseEvent) => {
       console.log('button clicked');
 
@@ -15,21 +17,28 @@ export const StartPage = defineComponent({
       console.log('onClickFloatButton clicked');
 
     }
+    const onClickNavIcon = (e: MouseEvent) => {
+      refVisibleOverlay.value = !refVisibleOverlay.value;
+    }
     return () => (
       <div>
         <Navbar>
           {{
-            default: ()=>'桃子记账',
-            icon: ()=><Icon name="menu" class={s.navIcon}/>,
+            default: () => '桃子记账',
+            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickNavIcon} />,
           }}
         </Navbar>
         <Center class={s.pig_wrapper}>
-          <Icon name={"pig"} class={s.pig}/>
-        </Center> 
+          <Icon name={"pig"} class={s.pig} />
+        </Center>
         <div class={s.button_wrapper}>
           <Button onClick={onClick}>开始记账</Button>
         </div>
-        <FloatButton iconName="add" onClick={onClickFloatButton}/>  
+        <FloatButton iconName="add" onClick={onClickFloatButton} />
+        {
+          refVisibleOverlay.value && <Overlay onClose={() => refVisibleOverlay.value = false} />
+        }
+
       </div>
     )
   }
