45f7d9867759a37b52ccfd774d3c158445f1665e
fredafei <964959033@qq.com>
2022-08-01 21:28:26 +0800

    添加layouts，优化start page

--- src/views/start/StartPage.tsx
+++ src/views/start/StartPage.tsx
@@ -1,10 +1,10 @@
-import { defineComponent, PropType, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { Button } from '../../components/button/Button';
 import { Center } from '../../components/center/Center';
 import { FloatButton } from '../../components/float-button/FloatButton';
 import { Icon } from '../../components/icon/Icon';
-import { Navbar } from '../../components/navbar/Navbar';
 import { Overlay } from '../../components/overlay/Overlay';
+import { MainLayout } from '../../layouts/MainLayout';
 import s from './StartPage.module.scss';
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -21,25 +21,24 @@ export const StartPage = defineComponent({
       refVisibleOverlay.value = !refVisibleOverlay.value;
     }
     return () => (
-      <div>
-        <Navbar>
-          {{
-            default: () => '桃子记账',
-            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickNavIcon} />,
-          }}
-        </Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name={"pig"} class={s.pig} />
-        </Center>
-        <div class={s.button_wrapper}>
-          <Button onClick={onClick}>开始记账</Button>
-        </div>
-        <FloatButton iconName="add" onClick={onClickFloatButton} />
-        {
-          refVisibleOverlay.value && <Overlay onClose={() => refVisibleOverlay.value = false} />
-        }
-
-      </div>
+      <MainLayout>
+        {{
+          title: () => '桃子记账',
+          icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickNavIcon} />,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name={"pig"} class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <Button onClick={onClick}>开始记账</Button>
+            </div>
+            <FloatButton iconName="add" onClick={onClickFloatButton} />
+            {
+              refVisibleOverlay.value && <Overlay onClose={() => refVisibleOverlay.value = false} />
+            }
+          </>
+        }}
+      </MainLayout>
     )
   }
 })
