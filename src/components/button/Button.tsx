679d80851aee72c2591c83dab08031c006a6ec54
fredafei <964959033@qq.com>
2024-02-01 21:09:16 +0800

    完善Button组件

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType, ref } from 'vue';
 import s from './Button.module.scss';
 
 interface Props {
@@ -15,11 +15,37 @@ export const Button = defineComponent({
     },
     type: {
       type: String as PropType<'submit' | 'button'>
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    autoSelfDisabled: {
+      type: Boolean,
+      default: false
     }
   },
   setup: (props, context) => {
+    const selfDisabled = ref(false)
+    const _disabled = computed(() => {
+      if(props.autoSelfDisabled === false){
+        return props.disabled
+      }
+      if(selfDisabled.value){
+        return true
+      }else{
+        return props.disabled
+      }
+    })
+    const onClick = (e: MouseEvent) => {
+      props.onClick?.(e)
+      selfDisabled.value = true
+      setTimeout(()=>{
+        selfDisabled.value = false
+      },500)
+    }
     return () => (
-      <button type={props.type} class={[s.button, s[props.level]]}>
+      <button disabled={_disabled.value} type={props.type} class={[s.button, s[props.level]]} onClick={onClick}>
         {context.slots.default?.()}
       </button>
     )
