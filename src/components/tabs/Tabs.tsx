d2ee48f4f72f33cb7c8fa92cbb972407700bf7c2
fredafei <964959033@qq.com>
2024-02-01 22:18:56 +0800

    完善tab 组件

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -6,40 +6,61 @@ export const Tabs = defineComponent({
       type: String
     },
     selected: {
-      type: String as PropType<string>
+      type: String as PropType<string>,
+      required: false,
     },
+    rerenderOnSelect: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    }
   },
   emits: ['update:selected'],
   setup: (props, context) => {
     return () => {
-      const tabs = context.slots.default?.();
-      if (!tabs) { return () => null }
-      for (let i = 0; i < Tabs.length; i++) {
-        if (tabs[i].type !== 'Tab') {
-          throw new Error('<Tabs/> only accepts <Tab/> as children');
+      const tabs = context.slots.default?.()
+      if (!tabs) return () => null
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].type !== Tab) {
+          throw new Error('<Tabs> only accepts <Tab> as children')
         }
       }
       const cp = props.classPrefix
-      return <div class={[s.tabs, cp ? (cp + '_tabs') : '']}>
-        <ol class={[s.tabs_nav, cp ? (cp + '_tabs_nav') : '']}>
+      return <div class={[s.tabs, cp + '_tabs']}>
+        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
           {tabs.map(item =>
             <li class={[
-              item.props?.name === props.selected ? [s.selected, cp ? (cp + '_selected') : ''] : '',
-              cp ? (cp + '_tabs_nav_item') : ''
+              item.props?.value === props.selected ? [s.selected, cp + '_selected'] : '',
+              cp + '_tabs_nav_item'
             ]}
-              onClick={() => context.emit('update:selected', item.props?.name)}
-            >{item.props?.name}</li>)
-          }
+              onClick={() => context.emit('update:selected', item.props?.value)}
+            >
+              {item.props?.name}
+            </li>)}
         </ol>
-        <div>{tabs.find(item => item.props?.name === props?.selected)}</div>
+        {props.rerenderOnSelect ?
+          <div key={props.selected}>
+            {tabs.find(item=>item.props?.value === props.selected)}
+          </div>:
+          <div>
+          {tabs.map(item =>
+            <div v-show={item.props?.value === props.selected}>{item}</div>
+          )}
+          </div>
+        }
       </div>
     }
   }
 })
+
 export const Tab = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
+      type: String as PropType<string>,
+      required: true
+    },
+    value: {
+      type: String as PropType<string>,
+      required: true
     }
   },
   setup: (props, context) => {
